d274b66ea646f1cf210541fc134f38eaf527e9f4
Nimtzwil <nimtzwil@msu.edu>
2014-05-05 18:58:13 +0200

    Array Dimension Fix again

--- geometry.f90
+++ geometry.f90
@@ -22,7 +22,7 @@ subroutine Initialize
 ! Main initilizing routine
     call GetParameters(NumPoints)
     
-    allocate(KnownPos(2, NumPoints), Dist(NumPoints(NumPoints)/2), Pos(2, NumPoints))
+    allocate(KnownPos(2, NumPoints), Dist(NumPoints*(NumPoints-1)/2), Pos(2, NumPoints))
     
 end subroutine Initialize
 
