2e081a74c2747e473c0a120479623f93961775c8
Soufian Lemkaddem <soufian_91@hotmail.com>
2014-05-05 17:34:11 +0200

    Constructed the first two points

--- core.f90
+++ core.f90
@@ -3,15 +3,30 @@ program Core
 	implicit none
 
 call Print
-call Initialize !Testing Changes 2
+!call Initialize !Testing Changes 2
+call Getpoint
 
 	
 contains
-!
+
 subroutine Print
 
 print*, 'hello World'
 
 end subroutine
+
+
+	
+subroutine Getpoint
+
+integer, parameter :: Ndist = 6
+real(8), parameter :: dist(Ndist) = (/1.0,2.0,3.0,5.0,6.0,2.0/)
+real(8) :: p(4,2)
+
+p(1,:) = (/0.0,0.0/)
+p(2,:) = (/dist(1),0.0/)
+
+end subroutine 
+
 
 end program
