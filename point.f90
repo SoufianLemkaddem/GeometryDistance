149c39a86e44eacf44887944175b16d5f67da16e
Soufian Lemkaddem <soufian_91@hotmail.com>
2014-05-07 14:14:56 +0200

    Added Alternative function

--- point.f90
+++ point.f90
@@ -35,4 +35,17 @@ dist = sqrt((p1(1) - p2(1))**2 + (p1(2) - p2(2))**2)
 
 end subroutine
 
+subroutine GetAlternativeP3(P4, p1, p2, p3)
+
+real(8), intent(in) :: p1(2), p2(2), p3(2)
+real(8), intent(inout) :: P4(2,3)
+
+P4(:,1) = p1(1)
+P4(:,2) = p2(1)
+P4(:,3) = p3(1)
+
+
+end subroutine
+
+
 end module
