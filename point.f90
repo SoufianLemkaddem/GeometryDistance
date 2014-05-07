4a45618ddf1ec26130fbf6b3a903943c691c1c3d
Soufian Lemkaddem <soufian_91@hotmail.com>
2014-05-07 14:42:31 +0200

    Added triangle inequality

--- point.f90
+++ point.f90
@@ -18,11 +18,15 @@ real(8), intent(in) :: disti, dist1, dist2
 real(8), intent(out) :: p3(2)
 real(8) :: angle, arg_angle
 
-arg_angle = (dist1**2 + disti**2 - dist2**2)/(2*disti*dist1)
-angle = acos(arg_angle)
+if (disti + dist1 > dist2 ) then
 
-p3(1) = cos(angle)*dist1
-p3(2) = sin(angle)*dist1
+	arg_angle = (dist1**2 + disti**2 - dist2**2)/(2*disti*dist1)
+	angle = acos(arg_angle)
+
+	p3(1) = cos(angle)*dist1
+	p3(2) = sin(angle)*dist1
+
+end if 
 
 end subroutine
 
@@ -35,14 +39,20 @@ dist = sqrt((p1(1) - p2(1))**2 + (p1(2) - p2(2))**2)
 
 end subroutine
 
-subroutine GetAlternativeP3(P4, p1, p2, p3)
+subroutine GetAlternativeP3(RefP, p1, p2, p3)
 
 real(8), intent(in) :: p1(2), p2(2), p3(2)
-real(8), intent(inout) :: P4(2,3)
+real(8), intent(inout) :: RefP(2,3)
 
-P4(:,1) = p1(1)
-P4(:,2) = p2(1)
-P4(:,3) = p3(1)
+RefP(2,1) = p3(2)
+RefP(1,2) = p2(1) - p3(1)
+
+RefP(1,1) = p3(1)
+RefP(2,1) = -p3(2)
+
+RefP(1,3) = p1(1)
+RefP(2,3) = p1(2)
+
 
 
 end subroutine
