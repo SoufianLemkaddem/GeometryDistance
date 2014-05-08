54ae0a74ff250141dfe6032a8c5ce09ac7c667f4
Nimtzwil <nimtzwil@msu.edu>
2014-05-08 11:37:41 +0200

    Fixed non-conforming tabs

--- test_points.f90
+++ test_points.f90
@@ -16,80 +16,63 @@ contains
 ! **********************************************************************************   
 ! Reads 'geometry.params'
 subroutine shapes
-	real*8, dimension(20,2) :: test_array, dist_array(120)
+    real*8, dimension(20,2) :: test_array, dist_array(120)
 
-	call randPoints(test_array, dist_array)
+    call randPoints(test_array, dist_array)
 
-	call geometricdraw(test_array)
-
-	
-
+    call geometricdraw(test_array)
 end subroutine shapes
 !**********************************************************************
 !
 ! Reads in the positions from a file of NumPoint lines
 subroutine randPoints(test_array, dist_array)
-	real*8, intent(inout) :: test_array(:,:), dist_array(:)
-	real*8 :: xCord, yCord
-	integer :: k
+    real*8, intent(inout) :: test_array(:,:), dist_array(:)
+    real*8 :: xCord, yCord
+    integer :: k
 
-
-	open(16, file = 'points.dat')
+    open(16, file = 'points.dat')
     do k = 1, 20
 
-	xCord = k*rand()
-	ycord = k*rand()
+        xCord = k*rand()
+        ycord = k*rand()
 
-	test_array(k,1) = xcord
-	test_array(k,2) = ycord
+        test_array(k,1) = xcord
+        test_array(k,2) = ycord
 
-	write(16,*) xcord, ycord
+        write(16,*) xcord, ycord
 	
-	!print*, xcord, ycord, rand()
+        !print*, xcord, ycord, rand()
 
     end do   
 
-	call FillDistKP(k, test_array, dist_array)
+    call FillDistKP(k, test_array, dist_array)
 
-	print*, dist_array
-	close(16) 
+    print*, dist_array
+    close(16) 
 end subroutine randPoints
 !**********************************************************************
 !Subroutine finddistance(test_array)
-!	real*8, intent(in) :: test_array(:,:)
-!	real*8, intent(out) :: dist_array(size(test_array(:,1))
+!    real*8, intent(in) :: test_array(:,:)
+!    real*8, intent(out) :: dist_array(size(test_array(:,1))
 !
-!	real*8, :: x1, y1, x2, y2, distance
+!    real*8, :: x1, y1, x2, y2, distance
 !
-!	integer :: i
-!	
-!	
+!    integer :: i
 !
-!	do i=1 , size(test_array(:,1))
+!    do i=1 , size(test_array(:,1))
 !
-!		x1 = test_array(i,1)
-!		y1 = test_array(i,2)
+!    x1 = test_array(i,1)
+!    y1 = test_array(i,2)
 !
-!		do j=1, size(test_array(:,1))
+!    do j=1, size(test_array(:,1))
 !
-!		y2 = test_array(j,2)
-!		x2 = test_array(j,1)
+!    y2 = test_array(j,2)
+!    x2 = test_array(j,1)
 !
-!		distance = sqrt((x2 - x1)**2 + (y2 - y1)**2)
+!    distance = sqrt((x2 - x1)**2 + (y2 - y1)**2)
 !
-!		dist_array(j)
+!    dist_array(j)
 !end do
 !end subroutine
-
-
-	
-	
-
-
-
-
-
-
-
 !**********************************************************************
 end module
